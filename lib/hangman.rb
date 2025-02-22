c81173b837dac106f4f4b5961894446f22f844c1
mconley91 <matthew.e.conley@gmail.com>
2025-02-22 17:32:36 -0500

    add draw_board, handle_turn, generate_secret_word Game class, attr_accessor

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,10 +1,34 @@
-def start_game
-    dictionary = File.read('google-10000-english-no-swears.txt').split(" ")
-    secret_word = ''
-    until word.length > 5 && word.length < 12
-      secret_word = dictionary.sample.chomp
+class Game
+    attr_accessor(:game_board, :turn, :secret_word, :guess)
+    def initialize
+        @game_board = []
+        @turn = 0
+        @secret_word = ''
+        @guess = ''
     end
-    secret_word
+
+    def generate_secret_word
+        dictionary = File.read('google-10000-english-no-swears.txt').split(" ")
+        word = ''
+        until word.length > 5 && word.length < 12
+            word = dictionary.sample.chomp
+        end
+        self.secret_word = word.upcase.split('')
+    end
+
+    def handle_turn
+        puts 'Enter a letter to decode the secret word!'
+        self.guess = gets.chomp
+    end
+
+    def draw_board
+        self.game_board = Array.new(secret_word.length, '_')
+    end
 end
 
-start_game
+current_game = Game.new()
+
+current_game.generate_secret_word
+current_game.draw_board
+p current_game.secret_word
+p current_game.game_board
