533e0a7f399f2203a36642f5e1e9bfe93178a71d
mconley91 <matthew.e.conley@gmail.com>
2025-02-22 19:56:58 -0500

    add save & load functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -17,18 +17,24 @@ class Game
     end
 
     def handle_turn
-        puts 'Enter a letter to decode the secret word!'
         self.guess = gets.chomp.upcase
-        if secret_word.include?(guess)
-          arr_of_indexes = []
-          secret_word.each_with_index do|letter, index| 
-            if letter == guess 
-            arr_of_indexes << index
-            end
-          end
-          arr_of_indexes.each {|indexer| self.game_board[indexer] = guess}
+        if guess == 'SAVE'
+          save_game
+        elsif guess == 'LOAD'
+            load_game
         else
-          puts "Incorrect guess! Tries Left: #{7 - (self.strikes += 1)}"
+            if secret_word.include?(guess)
+              arr_of_indexes = []
+              secret_word.each_with_index do|letter, index| 
+                if letter == guess 
+                arr_of_indexes << index
+                end
+              end
+              arr_of_indexes.each {|indexer| self.game_board[indexer] = guess}
+            else
+              puts "Incorrect guess! Tries Left: #{7 - (self.strikes += 1)}"
+            end
+            p self.game_board
         end
     end
 
@@ -50,14 +56,33 @@ class Game
     def play_game
         puts "START: #{game_board}"
         until is_winner_or_loser? do
-            puts "CHEAT: #{self.secret_word}"
+            # puts "CHEAT: #{self.secret_word}"
             self.handle_turn
-            p self.game_board
         end
+    end
+
+    def save_game
+      game_states = [game_board.join(''), strikes.to_s, secret_word.join(''), guess.to_s]
+      Dir.mkdir('saves') unless Dir.exist?('saves')
+      File.open('saves/saved_game.txt', 'w') do |file|
+        file.puts game_states
+      end
+      puts "Game Saved!"
+    end
+
+    def load_game
+      game_info = File.read('saves/saved_game.txt').split(/\n/)
+      self.game_board = game_info[0].split('')
+      self.strikes = game_info[1].to_i
+      self.secret_word = game_info[2].split('')
+      self.guess = game_info[3]
+      puts 'Game Loaded!'
+      p game_board
     end
 end
 
 current_game = Game.new()
 current_game.generate_secret_word
 current_game.draw_board
+puts 'Enter a letter to decode the secret word! Type SAVE or LOAD to save or load a previous session.'
 current_game.play_game
